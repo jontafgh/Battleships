47704e6809b1bedb99d817fbe424b4b1d4200236
jonatan.thunell@yahoo.com <jonatan.thunell@yahoo.com>
2024-09-14 13:17:35 +0200

    Working Ver

--- Battleships/Player.cs
+++ Battleships/Player.cs
@@ -4,16 +4,6 @@ using System.Threading;
 namespace Battleships
 
 {
-    public class ShipPartXY
-    {
-        public int[] X { get; set; }
-        public int[] Y { get; set; }
-        public ShipPartXY(int parts)
-        {
-            X = new int[parts];
-            Y = new int[parts];
-        }
-    }
     public class Player : IShipCaptain
     {
         public Map Map { get; private set; }
---
+++ Battleships/ShipPartXY.cs
@@ -0,0 +1,14 @@
+﻿namespace Battleships
+
+{
+    public class ShipPartXY
+    {
+        public int[] X { get; set; }
+        public int[] Y { get; set; }
+        public ShipPartXY(int parts)
+        {
+            X = new int[parts];
+            Y = new int[parts];
+        }
+    }
+}
